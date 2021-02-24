a36556f86e9ba5de47392d4d3e47f4c1b7db274c
Santiago Alessandro <saantialessa@gmail.com>
2021-02-24 08:39:26 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, ScrollView, StyleSheet, Text, View, Dimensions} from 'react-native';
+import { slide as Menu } from 'react-burger-menu'
 
 export default function App() {
+
+  showSettings (e) {
+    e.preventDefault();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}} >
+      <ScrollView>
+        <ImageBackground style={styles.image} source={require('./assets/heroimg.jpg')}>
+        <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+        </ImageBackground>
+        <View>
+          <Text>Aqui va a ir el cta!</Text>
+        </View>
+      </ScrollView>
     </View>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image: {
+    resizeMode: 'stretch',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerItem:{
+    color: 'white',
+    fontSize: 20,
+    paddingTop: 30
+  }
 });
